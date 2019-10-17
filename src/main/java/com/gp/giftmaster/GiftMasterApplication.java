6fcff94f74612aca5de6a5f1932e42c52be33979
Ganesh <Ganesh@DESKTOP-7POP5RF>
2019-10-17 23:26:48 +0530

    Ganesh

---
+++ src/main/java/com/gp/giftmaster/GiftMasterApplication.java
@@ -0,0 +1,13 @@
+package com.gp.giftmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GiftMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GiftMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gp/giftmaster/GiftMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gp.giftmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GiftMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
